052b2503fe4d73d128fcce45a7f5b5d952cf7a03
avikroytech <68196967+avikroytech@users.noreply.github.com>
2025-06-14 10:27:08 -0700

    Reorganized runtime files

--- plotting.py
+++ plotting.py
@@ -1,10 +1,10 @@
 from measure import measure_runtime_across_range
 from mplt import plot_func_runtimes
 
-from constant import constant
-from linear import linear
-from quadratic import quadratic
-from cubic import cubic
+from runtime_funcs.constant import constant
+from runtime_funcs.linear import linear
+from runtime_funcs.quadratic import quadratic
+from runtime_funcs.cubic import cubic
 
 runtimes = measure_runtime_across_range(cubic, 1, 100)
 plot_func_runtimes(runtimes, "CUBIC")
